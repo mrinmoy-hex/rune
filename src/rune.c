de9904946cbe1fe170ae7aff38773d346cc7b31c
mrinmoy-hex <init2350@gmail.com>
2026-06-20 11:09:39 +0530

    feat: add support for raw mode

--- src/rune.c
+++ src/rune.c
@@ -1,6 +1,28 @@
+#include <stdlib.h>
 #include <unistd.h>
+#include <termios.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);  // read current attributes into struct raw
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);     // turn off echo ()
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 
 int main() {
+    enableRawMode();
+
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
